7f249147b630272110d2ea07e3d5cf8f1222308b
Daniel Tse <danielts@bu.edu>
2022-08-18 17:27:43 -0500

    signin modal styled

--- together/src/components/LoginModal.js
+++ together/src/components/LoginModal.js
@@ -1,11 +1,18 @@
-import { Modal } from "semantic-ui-react";
+import { Modal, Button, Input } from "semantic-ui-react";
 import "../style/LoginModal.css";
 
 function LoginModal() {
     return (
-        <Modal open={true} size={"mini"}>
-            <Modal.Header className="modalBody">Welcome to Together!</Modal.Header>
-            <Modal.Content className="modalBody">Please Sign In</Modal.Content>
+        <Modal open={true} size={"mini"} style={{height:"350px"}}>
+            <Modal.Header className="textCenter">Welcome to Together!</Modal.Header>
+            <Modal.Content className="textCenter">Please Sign In</Modal.Content>
+            <div className="textCenter inputBoxes">
+                <Input className="inputs" placeholder="username"></Input>
+                <Input className="password inputs" placeholder="password"></Input>
+            </div>
+            <div className="textCenter signinButton">
+                <Button color="teal">Sign In</Button>
+            </div>
         </Modal>
     );
 }

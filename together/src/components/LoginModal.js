625ea8f767904892ad4dbca6e2f4e9f7721fbd99
Daniel Tse <danielts@bu.edu>
2022-08-18 00:35:35 -0500

    outline of signin box

--- together/src/components/LoginModal.js
+++ together/src/components/LoginModal.js
@@ -1,7 +1,12 @@
+import { Modal } from "semantic-ui-react";
+import "../style/LoginModal.css";
 
 function LoginModal() {
     return (
-        <div>Login Here</div>
+        <Modal open={true} size={"mini"}>
+            <Modal.Header className="modalBody">Welcome to Together!</Modal.Header>
+            <Modal.Content className="modalBody">Please Sign In</Modal.Content>
+        </Modal>
     );
 }
 
